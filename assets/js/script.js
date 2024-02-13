e87cf987158983a585c8a8eed6dd688ad313c01d
zubairasif3 <119746286+zubairasif3@users.noreply.github.com>
2024-02-13 20:51:46 +0500

    first-commit

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+
+jQuery("#team-carousel").owlCarousel({
+    autoplay: false,
+    rewind: false,
+    loop: true,
+    margin: 20, 
+    responsiveClass: true,
+    autoHeight: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 6
+      },
+  
+      1366: {
+        items: 6
+      }
+    }
+});
+jQuery("#full-carousel").owlCarousel({
+    autoplay: false,
+    rewind: false,
+    loop: true,
+    margin: 20, 
+    responsiveClass: true,
+    autoHeight: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 6
+      },
+  
+      1366: {
+        items: 6
+      }
+    }
+});
+jQuery("#video-carousel").owlCarousel({
+    autoplay: false,
+    rewind: false,
+    loop: true,
+    margin: 20, 
+    responsiveClass: true,
+    autoHeight: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+  
+      600: {
+        items: 2.8
+      },
+  
+      1024: {
+        items: 3.8
+      },
+  
+      1366: {
+        items: 3.8
+      }
+    }
+});
